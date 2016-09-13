a24bd021a732a4728d3cb0ce476860f52376ad71
Stanley Ndagi <ndagis@gmail.com>
2016-09-13 09:26:11 +0300

    Rename Hello World to Life Saver

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: ExtensionContext) {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        window.showInformationMessage('Hello World!');
+        window.showInformationMessage('Life Saver running!');
 
         var editor = window.activeTextEditor;
         if (!editor) {
