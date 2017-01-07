2773f56fc1d8f08255c97d43a5495532533b7da4
Stanley Ndagi <stan.ndagi@enigma.io>
2017-01-07 19:37:34 +0300

    Update extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
+'use strict';
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module
-import {commands, ExtensionContext, window, StatusBarAlignment}  from 'vscode';
+// Import the module and reference it with the alias vscode in your code below
+import { commands, ExtensionContext, window, StatusBarAlignment } from 'vscode';
 import * as bl from 'battery-level';
 
 // this method is called when your extension is activated
@@ -9,24 +10,22 @@ export function activate(context: ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "lifesaver" is now active!');
+    console.log('Congratulations, your extension "lifebuoy" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = commands.registerCommand('extension.sayHello', () => {
+    let disposable = commands.registerCommand('extension.saveLife', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        window.showInformationMessage('Life Saver running!');
+        window.showInformationMessage('Lifebouy is running on full screen!');
 
         var editor = window.activeTextEditor;
         if (!editor) {
-            return; // No open text editor
+            return;  // No open text editor
         }
 
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
         var bat = window.createStatusBarItem(StatusBarAlignment.Right);
 
         bl().then(lvl => {
