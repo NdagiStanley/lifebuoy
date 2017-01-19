6d284f0685809876e4ad6dfca51c8a48882addfa
NdagiStanley <stanley.ndagi@andela.com>
2017-01-19 14:49:09 +0300

    Add deactivation message

--- src/extension.ts
+++ src/extension.ts
@@ -49,4 +49,5 @@ export function activate(context: ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    window.showInformationMessage('Lifebouy is sad to see you leave!');
 }
